0b073fcb9500e2282173e8e2a40355b59764fccd
Fabian Adame <fabian.kinil@cbtis72.edu.mx>
2022-03-16 19:01:18 -0500

    login

--- app/src/main/java/com/cbtis/valorant/MainActivity.kt
+++ app/src/main/java/com/cbtis/valorant/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.cbtis.valorant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        //Login
+
+        val tNombre = findViewById<EditText>(R.id.User)
+        val tContraseña = findViewById<EditText>(R.id.Pasword)
+        val bIniciar = findViewById<Button>(R.id.bIngresar)
+
+        bIniciar.setOnClickListener {
+            val Usuario = tNombre.getText().toString()
+            val pass: String = tContraseña.getText().toString()
+            if (Usuario.equals("Fabian")&& pass.equals("123")){
+                /*
+                var intent = Intent(this,Generos::class.java)
+
+                startActivity(intent)*/
+                Toast.makeText(this,"Bienvenido!", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"Usuario o Contraseña incorrectos!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
     }
 }
